4de7027e949c97082baf6b8f14e99a2f5277922c
ySIDy <danilslipchenko2003@gmail.com>
2023-07-07 00:07:19 +0300

    1

--- PZ6/main.c
+++ PZ6/main.c
@@ -8,10 +8,10 @@ void main() {
 	SetConsoleOutputCP(1251);
 	char input[50];
 	fgets(input, sizeof(input), stdin);
-	int c1, c2, c3, c4;
+	float c1, c2, c3, c4;
 	sscanf_s(input, "%d %d %d %d", &c1, &c2, &c3, &c4);
-	int x = fabs(c1 - c3);
-	int y = fabs(c2 - c4);
+	float x = fabs(c1 - c3);
+	float y = fabs(c2 - c4);
 
 	float result = sqrt(pow(x, 2) + pow(y, 2));
 	printf("%5.6f\n", result);
